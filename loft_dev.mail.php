70cf0590857f392ef4107c605523e5202d1d2a6d
aklump <sourcecode@intheloftstudios.com>
2014-07-10 12:40:50 -0700

    Added loft_dev_mail_capture function to grab outbound emails in private dir.

--- loft_dev.mail.php
+++ loft_dev.mail.php
@@ -3,12 +3,17 @@
  * @file
  * This file simply tests the server's mail function
  */
-$to = 'music-williams@intheloftstudios.com';
-$from = 'music-williams@intheloftstudios.com';
+$to        = 'email@site.com';
+$from      = 'email@site.com';
 
-if (mail($to, 'testing server email', 'does the server send email?', 'from: '. $from)) {
+$headers   = array();
+$headers[] = 'From: ' . $from;
+$message   = 'Does this server send email?';
+$success   = mail($to, 'Testing server email', $message, implode("\r\n", $headers));
+if ($success) {
   print 'The server has sent an email to '. $to;
 }
 else {
   print 'The server COULD NOT SEND an email to '. $to;
 }
+exit;
