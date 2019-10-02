8f13abf6ea19b41cf290da67f410c513e9911704
Aaron Klump <sourcecode@intheloftstudios.com>
2019-10-02 10:11:38 -0700

    Add exception formatting for faster reading.

--- src/EventSubscriber/FinalExceptionSubscriber.php
+++ src/EventSubscriber/FinalExceptionSubscriber.php
@@ -14,6 +14,7 @@ class FinalExceptionSubscriber implements EventSubscriberInterface {
   public static function getSubscribedEvents() {
     // Run as the final (very late) KernelEvents::EXCEPTION subscriber.
     $events[KernelEvents::RESPONSE][] = ['prettifyExceptions', -256];
+
     return $events;
   }
 
@@ -29,6 +30,64 @@ class FinalExceptionSubscriber implements EventSubscriberInterface {
     $status = $response->getStatusCode();
     $type = $response->headers->get('Content-Type');
     if ($status >= 500 && $type === 'text/plain') {
+
+      $content = $response->getContent();
+      list($top, $backtrace) = explode('<pre class="backtrace">', $content);
+
+      $top = str_replace('The website encountered an unexpected error. Please try again later.</br></br>', '', $top);
+
+      $backtrace = trim(preg_replace('/<\/pre>$/', '', $backtrace));
+      $backtrace = explode(PHP_EOL, $backtrace);
+      $starting_count = count($backtrace) + 1;
+      $backtrace = '<ol>' . implode(PHP_EOL, array_map(function ($line) {
+          return "<li>$line</li>";
+        }, $backtrace)) . '</ol>';
+
+      $css = /** @lang css */
+        <<<EOD
+      .message {
+        margin: 0 20px;
+        font-size: 30px;
+        line-height: 1.5;
+        color: #333;
+      }
+      .backtrace {
+        margin-top: 2em;
+        font-size: 18px;
+        line-height: 3;
+        margin-left: 10px;
+        color: #0000cc;
+      }      
+      .backtrace ol{
+        list-style: none;
+        counter-reset: reverse {$starting_count};
+      }
+    
+      .backtrace ol> li:before {
+        content: counter(reverse) '.';
+        display: block;
+        left: -30px;
+        position: absolute;
+        text-align: right;
+        width: 20px;
+      }
+    
+      .backtrace ol>li {
+        counter-increment: reverse -1;
+        position: relative;
+      }
+      EOD;
+
+
+      $content = <<<EOD
+        <style type="text/css">{$css}</style>
+        <div class="message">{$top}</div>
+        <div class="backtrace">{$backtrace}</div>
+      EOD;
+
+      $response->setContent($content);
+
+
       $response->headers->remove('Content-Type');
       $response->headers->add([
         'Content-Type' => 'text/html',
